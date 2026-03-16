74faf13d6dbcaf37417a6f7fab385bf5561d64a5
koromodako <koromodako@gmail.com>
2026-03-16 22:44:13 +0100

    Add var/db/dslocal/nodes/Default/users/*.plist dissector

---
+++ dissectors/edf_plasma_dissectors/darwin/users.py
@@ -0,0 +1,59 @@
+"""Darwin Users Dissector"""
+
+from pathlib import Path
+
+from edf_plasma_core.concept import Tag
+from edf_plasma_core.dissector import (
+    DissectionContext,
+    Dissector,
+    register_dissector,
+)
+from edf_plasma_core.helper.selecting import select
+from edf_plasma_core.helper.table import Column, DataType
+from edf_plasma_core.helper.typing import PathIterator, RecordIterator
+
+from .helper import load_plist
+
+_GLOB_PATTERN = 'var/db/dslocal/nodes/Default/users/*.plist'
+
+
+def _select_impl(directory: Path) -> PathIterator:
+    yield from select(directory, _GLOB_PATTERN)
+
+
+def _get_value(data: dict, field: str) -> str:
+    return ','.join(data[field])
+
+
+def _dissect_impl(ctx: DissectionContext) -> RecordIterator:
+    data = load_plist(ctx.filepath)
+    yield {
+        'uid': _get_value(data, 'uid'),
+        'gid': _get_value(data, 'gid'),
+        'name': _get_value(data, 'name'),
+        'home': _get_value(data, 'home'),
+        'shell': _get_value(data, 'shell'),
+        'passwd': _get_value(data, 'passwd'),
+        'realname': _get_value(data, 'realname'),
+        'generateduid': _get_value(data, 'generateduid'),
+    }
+
+
+DISSECTOR = Dissector(
+    slug='darwin_users',
+    tags={Tag.DARWIN},
+    columns=[
+        Column('uid', DataType.INT),
+        Column('gid', DataType.INT),
+        Column('name', DataType.STR),
+        Column('home', DataType.STR),
+        Column('shell', DataType.STR),
+        Column('passwd', DataType.STR),
+        Column('realname', DataType.STR),
+        Column('generateduid', DataType.STR),
+    ],
+    description="System version information",
+    select_impl=_select_impl,
+    dissect_impl=_dissect_impl,
+)
+register_dissector(DISSECTOR)
