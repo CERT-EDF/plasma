7898eb7fc3adedb60d34c3ba40cec808e051de7f
koromodako <koromodako@gmail.com>
2026-03-16 21:16:47 +0100

    Add launch agent/daemon dissector

--- dissectors/edf_plasma_dissectors/darwin/__init__.py
+++ dissectors/edf_plasma_dissectors/darwin/__init__.py
@@ -2,5 +2,6 @@
 
 from . import (
     safari,
+    launch,
     version,
 )
---
+++ dissectors/edf_plasma_dissectors/darwin/launch.py
@@ -0,0 +1,64 @@
+"""Darwin Launch Configuration Dissector"""
+
+from pathlib import Path
+
+from edf_plasma_core.concept import Tag
+from edf_plasma_core.dissector import (
+    DissectionContext,
+    Dissector,
+    register_dissector,
+)
+from edf_plasma_core.helper.selecting import select
+from edf_plasma_core.helper.table import Column, DataType
+from edf_plasma_core.helper.typing import PathIterator, RecordIterator
+
+from .helper import load_plist
+
+
+_GLOB_PATTERN =  'Library/Launch*/*.plist'
+
+
+def _select_impl(directory: Path) -> PathIterator:
+    yield from select(directory, _GLOB_PATTERN)
+
+
+def _extract_argv(data: dict) -> list[str] | None:
+    argv = data.get('ProgramArguments')
+    if argv:
+        return argv
+    argv = data.get('Program')
+    if argv:
+        return [argv]
+    return None
+
+
+def _dissect_impl(ctx: DissectionContext) -> RecordIterator:
+    data = load_plist(ctx.filepath)
+    argv = _extract_argv(data)
+    if not argv:
+        ctx.register_error(f"argv not found in: {ctx.filepath}")
+        return
+    label = data.get('Label')
+    if not label:
+        ctx.register_error(f"label not found in: {ctx.filepath}")
+        return
+    yield {
+        'category': ctx.filepath.parent.name,
+        'label': label,
+        'argv': argv,
+    }
+
+
+DISSECTOR = Dissector(
+    slug='darwin_launch',
+    tags={Tag.DARWIN},
+    columns=[
+        Column('category', DataType.STR),
+        Column('label', DataType.STR),
+        Column('argv', DataType.STR),
+    ],
+    description="Darwin launch agents and daemons",
+    select_impl=_select_impl,
+    dissect_impl=_dissect_impl,
+)
+register_dissector(DISSECTOR)
